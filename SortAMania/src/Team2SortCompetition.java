bddb20abca006e1ea3bad6817ee5b9acb1834430
danaravvin <danaravvin@danas-air.home>
2017-12-08 06:39:57 -0500

    fixed messed up code

--- SortAMania/src/Team2SortCompetition.java
+++ SortAMania/src/Team2SortCompetition.java
@@ -3,25 +3,44 @@
 * SortAMania Team 2
 * 12/7/17
 */
+import java.util.Arrays;
 import java.util.Random;
 
-public class challenges {
+public class Team2SortCompetition extends SortCompetition{
 
 	//added merge, mergesort, create2Darray
-
+	
 	public static void main(String[] args)
 	{
-		//checks challengeOne
-		int[] arr1 = createArray(10000);
-		double median = challengeOne(arr1);
-		System.out.println("median: " + median);
-
-		//checks challengeTwo
-		String[] arr2 = {"hey", "hi", "bye"};
-		int index = challengeTwo(arr2, "bye");
-		System.out.println("index of string: " + index);
-
-		//checks challengeFour
+//		//checks challengeOne
+ 		//int[] arr1 = createArray(10000);
+ 		//System.out.println(challengeOne(arr1));
+// 		// WORKS
+// 		
+// 		//checks challengeTwo
+// 		String[] arr2 = {"hey", "hi", "bye"};
+// 		System.out.println(challengeTwo(arr2, "hi"));
+// 		// WORKS
+// 		
+ 		//checks challengeThree
+ 		//System.out.println(challengeThree(arr1));
+// 		// WORKS
+// 		
+// 		//checks challengeFour
+// 		int[][] arr3 = create2DArray(1000);
+// 		System.out.println(challengeFour(arr3));
+// 		// WORKS
+ 		
+// 		//checks challengeFive
+// 		String[] arr2 = {"hey", "hi", "bye"};
+// 		System.out.println(challengeFive(arr2, "hi"));
+// 		//	WORKS
+ 		
+	}
+	
+	public String greeting()
+	{
+		return "This is Katherin and Dana's sort entry.";
 	}
 
 	
@@ -30,12 +49,12 @@ public class challenges {
 	* @param arr - holds the median value 
 	* @return The integer value of the median of the array
 	*/
-	public static int challengeOne(int[] arr)
+	public int challengeOne(int[] arr)
 	{
-		printIntArray(arr);
+		//printIntArray(arr);
 		quickSort(arr, 0, arr.length-1);
-		printIntArray(arr);
-		return findMedian(arr);
+		//printIntArray(arr);
+		return (int) findMedian(arr);
 	}
 
 	/**
@@ -44,11 +63,11 @@ public class challenges {
 	* @param query - the string that is looked for in the arrays
 	* @return the position of the search word
 	*/
-	public static int challengeTwo(String[] arr, String query)
+	public int challengeTwo(String[] arr, String query)
 	{
-		printStringArray(arr);
-		bubbleSort(arr);
-		printStringArray(arr);
+		//printStringArray(arr);
+		mergeSort(arr);
+		//printStringArray(arr);
 
 		for (int i = 0; i < arr.length; i++)
 		{
@@ -66,7 +85,7 @@ public class challenges {
 	* @return the median of the array 
 	*/
 
-	public static int challengeThree(int[] arr)
+	public int challengeThree(int[] arr)
 	{
 		return challengeOne(arr);
 	}
@@ -76,18 +95,18 @@ public class challenges {
 	* @param arr - the two dimensional array to be sorted 
 	* @return the median of the medians in the two dimensional array
 	*/
-	public static int challengeFour(int[][] arr)
+	public int challengeFour(int[][] arr)
 	{
 		int[] arr2 = new int[arr.length];
 		for (int i = 0; i<arr.length; i++)
 		{
-			quickSort(arr[i], 0, arr[i].length);
+			challengeOne(arr[i]);
 
 			arr2[i] = (int) findMedian(arr[i]);
 		}
 		quickSort(arr2, 0, arr2.length);
 
-		return findMedian(arr2);
+		return (int)findMedian(arr2);
 	}
 
 	/** 
@@ -97,22 +116,30 @@ public class challenges {
 		* @return the position of the query in the array
 	*/
 
-	public static int challengeFive(Comparable[]arr, Comparable query)
+	public int challengeFive(Comparable[]arr, Comparable query)
 	{
-		return -1;
+		mergeSort2(arr);
+		 for (int i = 0; i<arr.length; i++)
+		 {
+			 if (arr[i].equals(query))
+		 	 {
+			 	return i;
+		 	 }
+		 }
+		 return 1;
 	}
 
 	//this function finds the median of the array
-	public static int findMedian(int[] arr)
+	public static double findMedian(int[] arr)
 	{
 		int middle = 0;
 		if (arr.length%2 == 0)
 		{
-			middle = (int)(arr[arr.length/2] + arr[(arr.length/2) - 1])/2;
+			middle = (arr[arr.length/2] + arr[(arr.length/2) - 1])/2;
 		}
 		else
 		{
-			middle = (int)arr[arr.length/2-1];
+			middle = arr[arr.length/2-1];
 		}
 		return middle;
 	}
@@ -131,6 +158,20 @@ public class challenges {
 		}
 		return arr;
 	}
+	
+	public static int[][] create2DArray(int n)
+	{
+		Random rnd = new Random();
+		int[][] arr = new int[n][n];
+	 	for (int i = 0; i < n; i++)
+	 	{
+	 		for (int j = 0; j < n; j++)
+	 		{
+				arr[i][j] = rnd.nextInt(10000);
+	 		}
+	 	}
+	 	return arr;
+	 }
 
 	/** 
 	* prints each number in the array
@@ -175,7 +216,7 @@ public class challenges {
 		int pivot = list1[front];
 		int pivPosition = front;
 
-		for (int i = front+1; i <= back; i++)
+		for (int i = front+1; i < back; i++)
 		{
 			if (list1[i]<=pivot)
 			{
@@ -195,7 +236,6 @@ public class challenges {
 	* @param list1- array to be sorted
 	* @param front- to be compared with
 	* @param back - for use of comparison 
-	* @return swapped string array
 	*/
 	public static void quickSort(int[] list1, int front, int back)
 	{
@@ -223,7 +263,7 @@ public class challenges {
 	//string sort
 	public static void bubbleSort(String[] list1)
 	{
-		for(int i = 0; i<list1.length-1; i++)
+		for(int i = 0; i<list1.length; i++) 
 		{
 			if(list1[i+1].compareTo(list1[i]) <= 0)
 			{
@@ -245,4 +285,146 @@ public class challenges {
 		arr[i] = arr[j];
 		arr[j] = temp;
 	}
+	
+	public static String[] merge(String[] list1, String[] list2)
+ 	{
+ 		String[] mergedList = new String[list1.length+list2.length];
+ 		
+ 		int listIndex = 0;
+ 		int i = 0;
+ 		int x = 0;
+ 		
+// 		String check1 = list1[i];
+// 		String check2 = list2[x];
+ 		
+		while(i < list1.length && x < list2.length)
+ 		{
+ 			//System.out.println("i: " + i + " x: " + x + " listIndex: " + listIndex + " check1: " + check1 + " check2: " + check2);
+ 			
+ 			if (list1[i].compareTo(list2[x]) <= 0)
+ 			{
+ 				//check1 is closer to a than check2
+ 				mergedList[listIndex] = list1[i];
+ 				listIndex++;
+ 				i++;
+ 			}
+ 			else
+ 			{
+ 				//check2 is closer to a than check1
+ 				mergedList[listIndex] = list2[x];
+ 				listIndex++;
+ 				x++;
+ 			}
+ 		}
+ 		
+ 		while (i < list1.length)
+ 		{
+ 			mergedList[listIndex] = list1[i];
+ 			i++;
+ 			listIndex++;
+ 		}
+ 		
+ 		while (x < list2.length)
+ 		{
+ 			mergedList[listIndex] = list2[x];
+ 			x++;
+ 			listIndex++;
+ 		}
+ 	 	return mergedList;
+ 	}
+	
+	public static String[] mergeSort(String[] arr)
+ 	{
+ 		//initializes the two halves
+// 		String[] list1 = new String[arr.length/2];
+// 		String[] list2 = new String[arr.length/2];
+ 		
+ 		//sets the values into the new arrays
+// 		for (int i = 0; i < arr.length/2; i++)
+// 		{
+// 			list1[i] = (String) arr[i];
+// 		}
+// 		for (int i = arr.length/2; i < arr.length; i++)
+// 		{
+// 			list2[i] = (String) arr[i];
+// 		}
+		
+		//incorporate Arrays.copyOfRange instead
+ 		
+ 		//completes the recursive separation
+		if (arr.length == 1)
+		{
+			return arr;
+		}
+		else
+		{
+	 		String[] list1 = Arrays.copyOfRange(arr, 0, arr.length/2);
+	 		String[] list2 = Arrays.copyOfRange(arr, arr.length/2, arr.length);
+	 		
+	 		return merge(mergeSort(list1), mergeSort(list2));		
+		}	
+ 	}
+	
+	public static Comparable[] merge2(Comparable[] list1, Comparable[] list2)
+ 	{
+		Comparable[] mergedList = new Comparable[list1.length+list2.length];
+ 		
+ 		int listIndex = 0;
+ 		int i = 0;
+ 		int x = 0;
+ 		
+// 		Comparable check1 = list1[i];
+// 		Comparable check2 = list2[x];
+ 		
+ 		while(i < list1.length && x < list2.length)
+ 		{
+ 			//System.out.println("i: " + i + " x: " + x + " listIndex: " + listIndex + " check1: " + check1 + " check2: " + check2);
+ 			
+ 			if (list1[i].compareTo(list2[x]) <= 0)
+ 			{
+ 				//check1 is closer to a than check2
+ 				mergedList[listIndex] = list1[i];
+ 				listIndex++;
+ 				i++;
+ 			}
+ 			else
+ 			{
+ 				//check2 is closer to a than check1
+ 				mergedList[listIndex] = list2[x];
+ 				listIndex++;
+ 				x++;
+ 			}
+ 		}
+ 		
+ 		while (i < list1.length)
+ 		{
+ 			mergedList[listIndex] = list1[i];
+ 			i++;
+ 			listIndex++;
+ 		}
+ 		
+ 		while (x < list2.length)
+ 		{
+ 			mergedList[listIndex] = list2[x];
+ 			x++;
+ 			listIndex++;
+ 		}
+ 		
+ 		return mergedList;
+ 	}
+	
+	public static Comparable[] mergeSort2(Comparable[] arr)
+ 	{
+		if (arr.length == 1)
+		{
+			return arr;
+		}
+		else
+		{
+	 		Comparable[] list1 = Arrays.copyOfRange(arr, 0, arr.length/2);
+	 		Comparable[] list2 = Arrays.copyOfRange(arr, arr.length/2, arr.length);
+	 		
+	 		return merge2(mergeSort2(list1), mergeSort2(list2));		
+		}	
+ 	}
 }
